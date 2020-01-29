6f5c0c9eba51f978d5b7de2b565e885c8d3a164d
Michael Kaufman <mkaufman94@gmail.com>
2020-01-29 08:53:11 -0600

    feat: Animating the Enemy

--- labyrinth-game/Scenes/Enemy.gd
+++ labyrinth-game/Scenes/Enemy.gd
@@ -3,7 +3,19 @@ extends Node2D
 var hp = 25 setget set_hp
 
 onready var hpLabel = $HPLabel
+onready var animationPlayer = $AnimationPlayer
+
+# Custom signal
+signal died
 
 func set_hp(new_hp):
 	hp = new_hp
 	hpLabel.text = str(hp) + "hp"
+	
+	if hp <= 0:
+		emit_signal("died")
+		queue_free()
+	else:
+		animationPlayer.play("Shake")
+		yield(animationPlayer, "animation_finished")
+		animationPlayer.play("Attack")

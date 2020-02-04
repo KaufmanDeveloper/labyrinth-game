34952f07f4b93d6d2e75474f79f8abdfeed1ff9c
Michael Kaufman <mkaufman94@gmail.com>
2020-02-04 08:42:11 -0600

    feat: Room transition and new enemies

--- labyrinth-game/Scenes/Battle.gd
+++ labyrinth-game/Scenes/Battle.gd
@@ -2,10 +2,19 @@ extends Node
 
 const BattleUnits = preload("res://BattleUnits.tres")
 
+export(Array, PackedScene) var enemies = []
+
 onready var battleActionButtons = $UI/BattleActionButtons
+onready var animationPlayer = $AnimationPlayer
+onready var nextRoomButton = $UI/CenterContainer/NextRoomButton
+onready var enemyPosition = $EnemyPosition
 
 func _ready():
+	randomize()
 	start_player_turn()
+	var enemy = BattleUnits.Enemy
+	if enemy != null:
+		enemy.connect("died", self, "_on_Enemy_died") # Dynamic signal connect
 
 func start_player_turn():
 	battleActionButtons.show()
@@ -23,5 +32,23 @@ func start_enemy_turn():
 		yield(enemy, "end_turn")
 	start_player_turn()
 
+func create_new_enemy():
+	enemies.shuffle()
+	var Enemy = enemies.front() # Scene itself, capitalize
+	var enemy = Enemy.instance() # Instance of scene, lowercase
+	enemyPosition.add_child(enemy)
+	enemy.connect("died", self, "_on_Enemy_died")
+
 func _on_Enemy_died():
+	nextRoomButton.show()
 	battleActionButtons.hide()
+
+func _on_NextRoomButton_pressed():
+	nextRoomButton.hide()
+	animationPlayer.play("FadeToNewRoom")
+	yield(animationPlayer, "animation_finished")
+	
+	var playerStats = BattleUnits.PlayerStats
+	playerStats.ap = playerStats.max_ap
+	battleActionButtons.show()
+	create_new_enemy()
--- labyrinth-game/Scenes/Enemy.gd
+++ labyrinth-game/Scenes/Enemy.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 const BattleUnits = preload("res://BattleUnits.tres")
 
-var hp = 25 setget set_hp
-var target = null
+export(int) onready var hp = 25 setget set_hp # Makes visible in Inspector
+export(int) var damage = 4
 
 onready var hpLabel = $HPLabel
 onready var animationPlayer = $AnimationPlayer
@@ -14,7 +14,8 @@ signal end_turn
 
 func set_hp(new_hp):
 	hp = new_hp
-	hpLabel.text = str(hp) + "hp"
+	if hpLabel != null:
+		hpLabel.text = str(hp) + "hp"
 
 func _ready():
 	BattleUnits.Enemy = self
@@ -29,7 +30,7 @@ func attack() -> void:
 	emit_signal("end_turn")
 
 func deal_damage():
-	BattleUnits.PlayerStats.hp -= 4
+	BattleUnits.PlayerStats.hp -= damage
 
 func take_damage(amount):
 	self.hp -= amount

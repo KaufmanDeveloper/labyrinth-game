3320fd53d1aed3016a3ff314588f19594b72eae7
Michael Kaufman <mkaufman94@gmail.com>
2020-02-09 13:44:25 -0600

    feat: Animations on dialogue and namebox

--- labyrinth-game/Dialogue/Dialogue.gd
+++ labyrinth-game/Dialogue/Dialogue.gd
@@ -4,6 +4,7 @@ onready var MusicPlayer = load("res://Music/MusicPlayer.tscn")
 onready var nameText = $UI/DialoguePanel/NamePanel/NameText
 onready var dialogueText = $UI/DialoguePanel/DialogueText
 onready var textSound = $TextSound
+onready var animationPlayer = $AnimationPlayer
 
 export (String, FILE, "*.json") var dialogue_file_path : String
 export (AudioStream) var track
@@ -13,6 +14,8 @@ signal proceed_dialogue
 var textTimer = 0
 var textRevealed = 0
 var dialogueIsRevealing = true
+var nameIsChecked = false
+var isInitialRender = true
 
 func _ready():
 	var musicPlayer = MusicPlayer.instance()
@@ -21,24 +24,10 @@ func _ready():
 	musicPlayer.play(0)
 	interact()
 
-func _process(delta):	
-	if dialogueIsRevealing:
-		if Input.is_action_just_pressed("ui_accept"):
-			textRevealed = dialogueText.get_text().length()
-		textTimer += 1
-		if textTimer >= 4:
-			textTimer = 0
-			if textRevealed < dialogueText.get_text().length():
-				if dialogueText.get_text().substr(textRevealed, 1) != " ":
-					textSound.play(0)
-				textRevealed += 1
-		if textRevealed >= dialogueText.get_text().length():
-			dialogueIsRevealing = false
-		dialogueText.set_visible_characters(textRevealed)
-	else:
-		if Input.is_action_just_pressed("ui_accept") and not dialogueIsRevealing:
-			reset_text_timers()
-			emit_signal("proceed_dialogue")
+func _process(delta):
+	if nameIsChecked:
+		type_text()
+
 
 func interact() -> void:
 	var dialogue : Dictionary = load_dialogue(dialogue_file_path)
@@ -65,13 +54,52 @@ func print_text(text):
 	dialogueText.set_text(text)
 
 func check_actor(name):
+	if nameText.get_text() == "Jem" and name == "Player" or (nameText.get_text() == name):
+		return
+	
+	nameText.set_text("")
+	nameIsChecked = false
+	
+	if not isInitialRender:
+		animationPlayer.play("NamePanelOut")
+		yield(animationPlayer, "animation_finished")
+		animationPlayer.play("DialogueBoxOut")
+		yield(animationPlayer, "animation_finished")
+	
+	isInitialRender = false
+	animationPlayer.play("DialogueBoxIn")
+	yield(animationPlayer, "animation_finished")
+	animationPlayer.play("NamePanelIn")
+	yield(animationPlayer, "animation_finished")
+	
 	# Will need to take array of actors and compare them to name field
 	if name == "Player":
 		nameText.set_text("Jem")
 	else:
 		nameText.set_text(name)
+	
+	nameIsChecked = true
 
 func reset_text_timers():
 	textTimer = 0
 	textRevealed = 0
 	dialogueIsRevealing = true
+
+func type_text():
+	if dialogueIsRevealing:
+		if Input.is_action_just_pressed("ui_accept"):
+			textRevealed = dialogueText.get_text().length()
+		textTimer += 1
+		if textTimer >= 4:
+			textTimer = 0
+			if textRevealed < dialogueText.get_text().length():
+				if dialogueText.get_text().substr(textRevealed, 1) != " ":
+					textSound.play(0)
+				textRevealed += 1
+		if textRevealed >= dialogueText.get_text().length():
+			dialogueIsRevealing = false
+		dialogueText.set_visible_characters(textRevealed)
+	else:
+		if Input.is_action_just_pressed("ui_accept") and not dialogueIsRevealing:
+			reset_text_timers()
+			emit_signal("proceed_dialogue")

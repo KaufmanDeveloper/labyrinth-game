7c8214ed794b4820f1442956cf52b84060711a29
Michael Kaufman <mkaufman94@gmail.com>
2020-02-09 11:37:26 -0600

    feat: SE import and text display

--- labyrinth-game/Dialogue/Dialogue.gd
+++ labyrinth-game/Dialogue/Dialogue.gd
@@ -1,6 +1,8 @@
 extends Node
 
 onready var MusicPlayer = load("res://Music/MusicPlayer.tscn")
+onready var nameText = $UI/DialoguePanel/NamePanel/NameText
+onready var dialogueText = $UI/DialoguePanel/DialogueText
 
 export (String, FILE, "*.json") var dialogue_file_path : String
 export (AudioStream) var track
@@ -15,6 +17,10 @@ func _ready():
 func interact() -> void:
 	var dialogue : Dictionary = load_dialogue(dialogue_file_path)
 	print(dialogue)
+	for index in dialogue:
+		var currentDialogue = dialogue[index]
+		check_actor(currentDialogue.name)
+		print_text(currentDialogue.text)
 	
 func load_dialogue(file_path) -> Dictionary:
 	# Parses a JSON file and returns it as a dictionary
@@ -25,3 +31,10 @@ func load_dialogue(file_path) -> Dictionary:
 	var dialogue = parse_json(file.get_as_text()) # Parse json into dictionary
 	assert(dialogue.size() > 0) # Ensure dialogue has length
 	return dialogue
+
+func print_text(text):
+	dialogueText.set_text(text)
+
+func check_actor(name):
+	# Will need to take array of actors and compare them to name field
+	nameText.set_text(name)

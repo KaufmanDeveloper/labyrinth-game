d47c33e5947609da91d6816035b74f808288fde7
Michael Kaufman <mkaufman94@gmail.com>
2020-02-12 07:59:51 -0600

    feat: Implement first and second choice variable

--- labyrinth-game/Decision/Decision.gd
+++ labyrinth-game/Decision/Decision.gd
@@ -2,10 +2,16 @@ extends Node
 
 onready var background = $Background
 onready var currentActor = $CurrentActor
+onready var firstChoiceButton = $UI/Panel/GridContainer/FirstChoiceButton
+onready var secondChoiceButton = $UI/Panel/GridContainer/SecondChoiceButton
 
 export (Texture) var backgroundTexture : Texture
 export (Texture) var currentActorTexture : Texture
+export (String) var firstChoice : String
+export (String) var secondChoice : String
 
 func _ready():
 	background.set_texture(backgroundTexture)
 	currentActor.set_texture(currentActorTexture)
+	firstChoiceButton.set_text(firstChoice)
+	secondChoiceButton.set_text(secondChoice)

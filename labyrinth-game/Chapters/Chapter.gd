2fda198e87288d2a67152dad0e99ea9efc9b4654
Michael Kaufman <mkaufman94@gmail.com>
2020-02-20 08:48:37 -0600

    feat: Lay out decisions tree resource

---
+++ labyrinth-game/Chapters/Chapter.gd
@@ -0,0 +1,9 @@
+extends Node
+
+export(Array, PackedScene) var elements = []
+
+const DecisionTree = preload("res://Globals/DecisionTree.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
---
+++ labyrinth-game/Globals/DecisionTree.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name DecisionTree
+
+# Track arrays of decisions made currently or have been made on save file
+var CurrentDecisionsTree = null
+var HistoricalDecisionsTree = null

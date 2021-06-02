469f966400b4a9e3c8c06f268e7aef9ebcc8192a
Michael Kaufman <mkaufman94@gmail.com>
2021-06-01 20:49:22 -0500

    feat: Change actor sprites to new sizing and structure (#23)
    * feat: Create base and test actor
    
    * feat: Test two frame actor
    
    * feat: Import Marcus and create Marcus actor
    
    * feat: Fix Name Panel animation and make dialogue box smaller
    
    * feat: Add Tara sprites
    
    * feat: Small fix and json correction
    
    * fix: Testing widths
    
    * feat: Complete updated design implementation for actors

--- labyrinth-game/Dialogue/Dialogue.gd
+++ labyrinth-game/Dialogue/Dialogue.gd
@@ -12,6 +12,7 @@ onready var textOutSound = $TextOut
 onready var animationPlayer = $AnimationPlayer
 onready var spriteFadesAnimationPlayer = $SpriteFadesAnimationPlayer
 onready var currentActor = $CurrentActor
+onready var currentActor2 = $CurrentActor2
 
 export (String, FILE, "*.json") var dialogue_file_path : String
 export (PackedScene) var background = PackedScene.new()
@@ -159,7 +160,7 @@ func reset_text_timers():
 
 func type_text():
 	if dialogueIsRevealing:
-		if currentActor and (not animationPlayer.current_animation == "Talking") and currentActorName != "Player":
+		if currentActor and currentActor2 and (not animationPlayer.current_animation == "Talking") and currentActorName != "Player":
 			animationPlayer.play("Talking")
 		elif animationPlayer.current_animation == "Talking" and currentActorName == "Player":
 			animationPlayer.stop()
@@ -179,7 +180,8 @@ func type_text():
 	else:
 		if animationPlayer.current_animation == "Talking":
 			animationPlayer.stop()
-			currentActor.set_frame(0)
+			currentActor.visible = true;
+			currentActor2.visible = false;
 		
 		if Input.is_action_just_pressed("ui_accept") and not dialogueIsRevealing:
 			reset_text_timers()
@@ -201,14 +203,18 @@ func load_actors(dialogue):
 	if (actors.size()):
 		currentActor.set_texture(actors[0].get_node("ActorSprite").get_texture())
 		currentActor.set_actorName(actors[0].actorName)
+		currentActor2.set_texture(actors[0].get_node("ActorSprite2").get_texture())
+		currentActor2.set_actorName(actors[0].actorName)
 
 func fade_in_actor(name):
 	for actor in actors:
 		if name == actor.actorName:
 			currentActor.set_texture(actor.get_node("ActorSprite").get_texture())
+			currentActor2.set_texture(actor.get_node("ActorSprite2").get_texture())
 			spriteFadesAnimationPlayer.play("FadeInActor")
 			yield(spriteFadesAnimationPlayer, "animation_finished")
 			currentActor.set_actorName(name)
+			currentActor2.set_actorName(name)
 			return
 
 func fade_out_actor(name):
@@ -217,6 +223,7 @@ func fade_out_actor(name):
 			spriteFadesAnimationPlayer.play("FadeOutActor")
 			yield(spriteFadesAnimationPlayer, "animation_finished")
 			currentActor.set_actorName(name)
+			currentActor2.set_actorName(name)
 			return
 
 func change_actor(name):
@@ -231,10 +238,12 @@ func change_actor(name):
 			spriteFadesAnimationPlayer.play("FadeOutActor")
 			yield(spriteFadesAnimationPlayer, "animation_finished")
 			currentActor.set_texture(actor.get_node("ActorSprite").get_texture())
+			currentActor2.set_texture(actor.get_node("ActorSprite2").get_texture())
 			yield(get_tree().create_timer(0.4), "timeout")
 			spriteFadesAnimationPlayer.play("FadeInActor")
 			yield(spriteFadesAnimationPlayer, "animation_finished")
 			currentActor.set_actorName(name)
+			currentActor2.set_actorName(name)
 			return
 
 func initiate_battle(name):
